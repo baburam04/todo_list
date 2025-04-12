22bfc58edcbd484e3a3129e8f5cd1287d4c34b46
baburam04 <baburam07sg@gmail.com>
2025-04-12 13:24:13 +0800

    Updated jwt.js

--- config/jwt.js
+++ config/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const ErrorResponse = require('../utils/errorResponse');
-const asyncHandler = require('./async');
+const asyncHandler = require('./middleware/async');
 const User = require('../models/User');
 
 // Set JWT secret and expiration
